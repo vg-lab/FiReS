d13d428ef5df98b7efddcb72f2221fa35a4e37fd
Pablo Toharia <pablo.toharia@urjc.es>
2015-09-23 09:17:12 +0200

    Added the option to round, floor or ceil the feature for casting it to int.

--- fires/FeatureCaster.h
+++ fires/FeatureCaster.h
@@ -22,7 +22,14 @@ namespace fires
     {
     }
 
-    virtual int toInt( Feature& ) = 0;
+    typedef enum
+    {
+      ROUND = 0,
+      CEIL,
+      FLOOR
+    } TIntCasting;
+
+    virtual int toInt( Feature&, TIntCasting casting = ROUND ) = 0;
 
 
   };
@@ -36,9 +43,22 @@ namespace fires
     {
     }
 
-    virtual int toInt( Feature& feature )
+    virtual int toInt( Feature& feature, TIntCasting casting = ROUND )
     {
-      return int( feature.value< T >( ));
+      switch ( casting )
+      {
+      case ROUND:
+        return int( round( feature.value< T >( )));
+        break;
+      case CEIL:
+        return int( ceil( feature.value< T >( )));
+        break;
+      case FLOOR:
+        return int( floor( feature.value< T >( )));
+        break;
+      default:
+        throw std::runtime_error( "Invalid casting type" );
+      }
     }
 
   };
