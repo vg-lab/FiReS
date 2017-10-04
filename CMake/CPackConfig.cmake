cb7e1494f2997dc12dd8e133256dc78ba1fdf6dd
Pablo Toharia <pablo.toharia@upm.es>
2017-10-04 18:03:03 +0200

    CPack fixes and cleanups.

--- CMake/CPackConfig.cmake
+++ CMake/CPackConfig.cmake
@@ -1,6 +1,6 @@
 set(CPACK_PACKAGE_CONTACT "Pablo Toharia <pablo.toharia@upm.es>")
 set(CPACK_PACKAGE_DESCRIPTION_FILE "${PROJECT_SOURCE_DIR}/README.md")
 
-set(CPACK_DEBIAN_PACKAGE_DEPENDS "")
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "libboost-dev")
 
 include(CommonCPack)
